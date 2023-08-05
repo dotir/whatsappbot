bf7cc5c3508e1c92a032b88c407c9f67f738363c
Amy2000gar <amy2000gar@gmail.com>
2023-08-05 14:27:53 -0500

    cambios3

--- app.js
+++ app.js
@@ -65,79 +65,130 @@ const flowPrincipal = addKeyword(['hola', 'consulta', 'buenas'])
             '👉 *Tiktok*  https://www.tiktok.com/@icomputec',
             '👉 *Instagram* https://www.instagram.com/icomputec/',
             '👉 *Pagina Web* https://icomputec.com/',
-
-        ]
-        .addAnswer('Escriba la opción del servicio que desea', {capture:true},(ctx) => {
-
-            console.log('Mensaje entrante: ', ctx.body);
-            switch (ctx.body) {
-                case '1':
-                  console.log(['Puede traer su equipo a nuestra tienda para hacele un diagnostico y indicarle la mejor solución,'+
-                  'lo esperamos en nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
-                  break;
-                case '2':
-                  console.log(['Desde 60 soles para laptop dependiendo del modelo de su equipo y desde 50 soles para CPU. Utilizamos pasta térmica de marca thermaltake, corsarir, '+
-                  'cooler master. El mantenimiento consiste en limpieza interna y externa del equipo, limpieza '+
-                  'del sistema de enfriamiento, limpieza del ventilador, cambio de pasta al procesador y tarjeta de video',
-                  'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
-                  break;
-                
-                case '3':
-                    console.log(['Si, podemos ayudarte', 'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
-                    break;
-                case '4':
-                    console.log(['El servicio de recuperación:'+
-                    '-Se realiza un diagnostico el cual tiene un costo de S/20.00 en el cual se verifica si es posible o no recuperar la información,' +
-                    ' en el caso  de que se pudiera recuperar la información, tendria un costo desde los S/100.00 dependiendo a la cantidad de datos.',
-                    'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
-                    break;
-                case '5':
-                    console.log('Si, te podemos ayudar. Nosotros repotenciamos y te podemos asesorar con lo mejor para su equipo', 'para mayor información' +
-                    'te recomendamos acercarte a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌')
-                    break;
-                default:
-                  console.log('Podria indicarnos su consulta.', 'En breve nuestro personal le atenderá');
-              }
-        }),
-        
+        ],
         null,
         null,
     )
+    .addAnswer('Escriba la opción del servicio que desea', {capture:true},(ctx) => {
+
+        console.log('Mensaje entrante: ', ctx.body);
+        switch (ctx.body) {
+            case '1':
+              console.log(['Puede traer su equipo a nuestra tienda para hacele un diagnostico y indicarle la mejor solución,'+
+              'lo esperamos en nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
+              break;
+            case '2':
+              console.log(['Desde 60 soles para laptop dependiendo del modelo de su equipo y desde 50 soles para CPU. Utilizamos pasta térmica de marca thermaltake, corsarir, '+
+              'cooler master. El mantenimiento consiste en limpieza interna y externa del equipo, limpieza '+
+              'del sistema de enfriamiento, limpieza del ventilador, cambio de pasta al procesador y tarjeta de video',
+              'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
+              break;
+            
+            case '3':
+                console.log(['Si, podemos ayudarte', 'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
+                break;
+            case '4':
+                console.log(['El servicio de recuperación:'+
+                '-Se realiza un diagnostico el cual tiene un costo de S/20.00 en el cual se verifica si es posible o no recuperar la información,' +
+                ' en el caso  de que se pudiera recuperar la información, tendria un costo desde los S/100.00 dependiendo a la cantidad de datos.',
+                'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌']);
+                break;
+            case '5':
+                console.log('Si, te podemos ayudar. Nosotros repotenciamos y te podemos asesorar con lo mejor para su equipo', 'para mayor información' +
+                'te recomendamos acercarte a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌')
+                break;
+            default:
+              console.log('Podria indicarnos su consulta.', 'En breve nuestro personal le atenderá');
+          }
+    })
     
 const flowPago = addKeyword(['tarjeta', 'efectivo', 'pago', 'pagos', 'transferencia'])
-.addAnswer('Aceptamos pagos en efectivo, todas las tarjetas con un 5% de recargo al total del servicio, billeteras digitales como Yape y Plin o transferencias bancarias')
+    .addAnswer('Aceptamos pagos en efectivo, todas las tarjetas con un 5% de recargo al total del servicio, billeteras digitales como Yape y Plin o transferencias bancarias')
+
 const flowCuentas = addKeyword(['cuentas', 'cuenta', 'cuenta de ahorro'])
-.addAnswer(['🟣 YAPE: 930276300 🟣\n','🟣 PLIN: 930276300 🟣\n'+
-'✅WILIAN LLANO\n'+
-'===========================\n'+
-'NÚMEROS DE CUENTA:\n'+
-'CUENTA - BCP SOLES:\n '+	
-'✅WILIAN LLANO\n	'	+
+    .addAnswer(['🟣 YAPE: 930276300 🟣\n','🟣 PLIN: 930276300 🟣\n'+
+                '✅WILIAN LLANO\n'+
+                '===========================\n'+
+                'NÚMEROS DE CUENTA:\n'+
+                'CUENTA - BCP SOLES:\n '+	
+                '✅WILIAN LLANO\n	'	+
 
-'21526721802015\n	'	+	
-'===========================\n'+
-'CUENTA - INTERBANK SOLES:\n '+
-'✅WILIAN LLANO\n'+
+                '21526721802015\n	'	+	
+                '===========================\n'+
+                'CUENTA - INTERBANK SOLES:\n '+
+                '✅WILIAN LLANO\n'+
 
-'3003050039857\n	'+
-'===========================\n'+
-'CUENTA - BBVA SOLES:\n '+
-'✅WILIAN LLANO\n'			+
+                '3003050039857\n	'+
+                '===========================\n'+
+                'CUENTA - BBVA SOLES:\n '+
+                '✅WILIAN LLANO\n'			+
 
-'0011-0764-0200096600\n'+
-'===========================\n'+
-'CUENTA - SCOTIABANK SOLES:\n' +
-'✅WILIAN LLANO\n	'		+
+                '0011-0764-0200096600\n'+
+                '===========================\n'+
+                'CUENTA - SCOTIABANK SOLES:\n' +
+                '✅WILIAN LLANO\n	'		+
 
-'7008344058\n'+
-'==========================='])
+                '7008344058\n'+
+                '==========================='])
+
+
 const flowHorario = addKeyword(['horario', 'atencion','atienden','hora', 'atendiendo', 'hora de atencion'])
-.addAnswer('HORARIO DE ATENCIÓN\n'+
-'Lunes a Viernes: 🕙 9:30 am - 1:30 pm 🕕\n'+
-'🕙 2:00 pm - 7:00 pm 🕕\n'+
-'Sábados: 🕙 10:00 am - 1:30 pm 🕑\n'+
- '🕙 2:00 pm - 6:00 pm 🕕\n'+
-'Domingo: Cerrado')
+    .addAnswer('HORARIO DE ATENCIÓN\n'+
+    'Lunes a Viernes: 🕙 9:30 am - 1:30 pm 🕕\n'+
+    '🕙 2:00 pm - 7:00 pm 🕕\n'+
+    'Sábados: 🕙 10:00 am - 1:30 pm 🕑\n'+
+    '🕙 2:00 pm - 6:00 pm 🕕\n'+
+    'Domingo: Cerrado')
+
+const flowDireccion = addKeyword(['ubicacion', 'direccion', 'ubica'])
+    .addAnswer('¡Hola!😊\n'+
+    'Bienvenid@ a ICOMPUTEC.COM👨🏻‍💻\n'+
+    '¡Muchas gracias por preferirnos!\n '+
+    'Esperamos poder servirte pronto.\n'+
+    
+        '🟡 VENTA DE LAPTOS 🟡\n'+
+    'ACCESORIOS y COMPUTADORAS\n'+
+    '___________'+
+      '⛔ SEDE PRINCIPAL ⛔\n'+
+    'https://maps.app.goo.gl/3FXcYqY3VME2FfKf6\n'+      
+    
+     'Octavio Muñoz Najar 221\n'+
+       'Galerias NOVA CENTER\n'+
+     '✅ Segundo Piso:  Tienda 214 - PRINCIPAL\n'+
+    
+     '✅ Tercer Piso:  Tienda 308\n'+
+    '___________\n'+
+    '🆘 SERVICIO TÉCNICO ESPECIALIZADO🆘\n'+
+             
+    '📲 930 276 300 - Segundo Piso\n '+
+    '📲 958 992 009 - Tercer Piso\n '+
+    '___________\n'+
+    'FACEBOOK\n'+
+    'https://www.facebook.com/icomputec/\n'+
+    '__________\n'+
+    '⚠️ HORÁRIO DE ATENCIÓN ⚠️\n'+
+    'Lunes a Viernes: 🕙 9:30 am - 1:30 pm 🕕\n'+
+    '🕙 2:00 pm - 7:00 pm 🕕\n'+
+    'Sábados: 🕙 10:00 am - 1:30 pm 🕑\n'+
+    '🕙 2:00 pm - 6:00 pm 🕕\n'+
+    '__________\n'+
+    'CONTACTE POR WHATSAPP\n'+
+     '📱https://wa.me/51930276300\n'+
+     '📱https://wa.me/51958992009\n'+
+    '___________\n'+
+    '🟡 NUESTROS ESPECIALISTAS 🟡\n'+
+          '👨🏻‍💻 Wilian Llano - Gerente\n'+
+          '👨🏻‍💻 David Acero\n'+
+          '👨🏻‍💻 Percy Aguilar\n'+
+          '👩‍💻 Amy Garcia\n'+
+          '👩‍💻 Karen Rivera\n'+
+    
+     '___________\n'+
+                'Desde 2006 - 2023\n'+
+              'https://icomputec.com\n'+
+     '___________\n'+
+    
+    'Saludos cordiales!')
 
 const main = async () => {
     const adapterDB = new MockAdapter()
