cbe3706b704eb782e66b60e3c0b56f7cb2b76e79
dotir <irvingllerena@gmail.com>
2023-08-06 23:43:50 -0500

    fix error

--- app.js
+++ app.js
@@ -119,20 +119,20 @@ const flowPrincipal = addKeyword(['hola', 'consulta', 'buenas'])
             case '2':
                 return endFlow({body:'Desde 60 soles para laptop dependiendo del modelo de su equipo y desde 50 soles para CPU. Utilizamos pasta térmica de marca thermaltake, corsarir, '+
                 'cooler master. El mantenimiento consiste en limpieza interna y externa del equipo, limpieza '+
-                'del sistema de enfriamiento, limpieza del ventilador, cambio de pasta al procesador y tarjeta de video',
+                'del sistema de enfriamiento, limpieza del ventilador, cambio de pasta al procesador y tarjeta de video'+
                 'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌'})
             case '3':
-                return endFlow({body:'Si, podemos ayudarte', 'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌'})
+                return endFlow({body:'Si, podemos ayudarte\n'+ 'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌'})
             case '4':
                 return endFlow({body:'El servicio de recuperación:'+
                 '-Se realiza un diagnostico el cual tiene un costo de S/20.00 en el cual se verifica si es posible o no recuperar la información,' +
-                ' en el caso  de que se pudiera recuperar la información, tendria un costo desde los S/100.00 dependiendo a la cantidad de datos.',
+                ' en el caso  de que se pudiera recuperar la información, tendria un costo desde los S/100.00 dependiendo a la cantidad de datos.\n'+
                 'Puede traer su equipo a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌'})
             case '5':
-                return endFlow({body:'Si, te podemos ayudar. Nosotros repotenciamos y te podemos asesorar con lo mejor para su equipo', 'para mayor información' +
+                return endFlow({body:'Si, te podemos ayudar. Nosotros repotenciamos y te podemos asesorar con lo mejor para su equipo\n'+ 'para mayor información' +
                 'te recomendamos acercarte a nuestra tienda: Octavio Muñoz Najar 221 Segundo Piso:  Tienda 214 - Galerias NOVA CENTER 📌'})
             default:
-                return endFlow({body:'Podria indicarnos su consulta.', 'En breve nuestro personal le atenderá'})
+                return endFlow({body:'Podria indicarnos su consulta.\n'+ 'En breve nuestro personal le atenderá'})
         }
     })
 
